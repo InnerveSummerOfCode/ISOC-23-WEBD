5eade1c6714638a69b95181de3983b12fbe8429e
ANUSHREE BONDIA <122273783+Anushreebasics@users.noreply.github.com>
2023-09-19 15:58:08 +0530

    Add files via upload

---
+++ isoc.js
@@ -0,0 +1,51 @@
+let navbarMenu = document.querySelector('.navbar-menu');
+let dropdownIsOpen = false;
+
+// Handle dropdown menu toggle
+navbarMenu.addEventListener('click', (event) => {
+  if (event.target.classList.contains('dropdown-toggler')) {
+    let target = document.querySelector(`#${event.target.dataset.dropdown}`);
+
+    if (target) {
+      if (target.classList.contains('show')) {
+        target.classList.remove('show');
+        dropdownIsOpen = false;
+      } else {
+        target.classList.add('show');
+        dropdownIsOpen = true;
+      }
+    }
+  }
+});
+
+// Handle closing dropdowns if a user clicks outside
+document.body.addEventListener('click', (event) => {
+  if (dropdownIsOpen) {
+    navbarMenu.querySelectorAll('.dropdown').forEach((dropdown) => {
+      let targetIsDropdown = dropdown == event.target;
+      let clickedOnDropdownToggle = event.target.classList.contains('dropdown-toggler');
+
+      if (clickedOnDropdownToggle) {
+        return;
+      }
+
+      if (!targetIsDropdown && !dropdown.contains(event.target)) {
+        dropdown.classList.remove('show');
+      }
+    });
+  }
+});
+
+// Open links in mobiles
+function handleSmallScreens() {
+  document.querySelector('.navbar-toggler').addEventListener('click', () => {
+    if (!navbarMenu.classList.contains('active')) {
+      navbarMenu.classList.add('active');
+    } else {
+      navbarMenu.classList.remove('active');
+    }
+  });
+}
+
+handleSmallScreens();
+
